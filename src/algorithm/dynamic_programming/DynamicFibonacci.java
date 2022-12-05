b7e77b6261bc0f1b347fec2fee72fc9687967d72
Arshad Ahmad <itsarshadahmad@gmail.com>
2022-12-05 21:39:34 +0530

    fibonacci series using dynamic programming added

---
+++ src/algorithm/dynamic_programming/DynamicFibonacci.java
@@ -0,0 +1,24 @@
+package src.algorithm.dynamic_programming;
+
+import java.util.HashMap;
+
+public class DynamicFibonacci {
+    private static final HashMap<Integer, Integer> cache = new HashMap<>();
+
+    public static int fibonacci(int num) {
+        if (cache.containsKey(num)) {
+            return cache.get(num);
+        }
+        if (num < 2) {
+            return num;
+        }
+        cache.put(num, fibonacci(num - 1) + fibonacci(num - 2));
+        return cache.get(num);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(14));
+        System.out.println(fibonacci(6));
+        System.out.println(fibonacci(12));
+    }
+}
