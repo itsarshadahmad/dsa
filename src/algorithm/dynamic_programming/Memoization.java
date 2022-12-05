5c08878bc06a03b7b63d178cf618854d3e5a617f
Arshad Ahmad <itsarshadahmad@gmail.com>
2022-12-05 21:06:12 +0530

    memoization added

---
+++ src/algorithm/dynamic_programming/Memoization.java
@@ -0,0 +1,25 @@
+package src.algorithm.dynamic_programming;
+
+import java.util.HashMap;
+
+public class Memoization {
+    private static final HashMap<Integer, Integer> cache = new HashMap<>();
+
+    public static int addTo80(int num) {
+        return num + 80;
+    }
+
+    public static int memoizeAddTo80(int num) {
+        if (!cache.containsKey(num)) {
+            System.out.println("Calculating");
+            cache.put(num, addTo80(num));
+        }
+        return cache.get(num);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(memoizeAddTo80(5));
+        System.out.println(memoizeAddTo80(5));
+        System.out.println(memoizeAddTo80(5));
+    }
+}
