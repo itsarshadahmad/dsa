be04994387f54bff1e97b9dc9c033d554e6d4043
Arshad Ahmad <arshadahmad.dev@gmail.com>
2023-04-25 18:32:01 +0530

    basics -> introductory questions added

---
+++ src/basics/introductory_questions/SwitchStatement.java
@@ -0,0 +1,35 @@
+package src.basics.introductory_questions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+Java Switch Case statement:
+https://practice.geeksforgeeks.org/problems/java-switch-case-statement3529/1
+ */
+public class SwitchStatement {
+    static double switchCase(int choice, List<Double> arr) {
+        return switch (choice) {
+            case 1 -> Math.PI * arr.get(0) * arr.get(0);
+            case 2 -> arr.get(0) * arr.get(1);
+            default -> 0;
+        };
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter choice 1 or 2: ");
+        int choice = scan.nextInt();
+        ArrayList<Double> list = new ArrayList<>();
+
+        System.out.println("Enter numbers: ");
+        list.add(scan.nextDouble());
+        if (choice != 1) {
+            list.add(scan.nextDouble());
+        }
+
+        double result = switchCase(choice, list);
+        System.out.println(result);
+    }
+}
