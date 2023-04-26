297d4830a50281a2c43b1ba380a110138f63af7e
Arshad Ahmad <arshadahmad.dev@gmail.com>
2023-04-26 22:04:27 +0530

    pattern solutions & introductory questions added

---
+++ src/basics/patterns/Pattern3.java
@@ -0,0 +1,28 @@
+package src.basics.patterns;
+
+public class Pattern3 {
+    /*
+    Pattern 3:
+    https://practice.geeksforgeeks.org/problems/triangle-number/1
+
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+    1 2 3 4 5
+
+     */
+
+    static void printTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + 1 + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printTriangle(5);
+    }
+}
---
+++ src/basics/patterns/Pattern5.java
@@ -0,0 +1,29 @@
+package src.basics.patterns;
+
+public class Pattern5 {
+    /*
+    Pattern 5:
+    https://practice.geeksforgeeks.org/problems/triangle-pattern/1
+
+     * * * * *
+     * * * *
+     * * *
+     * *
+     *
+
+     */
+
+    static void printTriangle(int n) {
+        // code here
+        for (int i = n; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printTriangle(5);
+    }
+}
