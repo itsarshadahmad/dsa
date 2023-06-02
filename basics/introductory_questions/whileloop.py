e80313648c5dbd96d1072d1fc6d042b0e69b253e
Arshad Ahmad <arshadahmad.dev@gmail.com>
2023-06-02 20:59:11 +0530

    while loop completed

---
+++ basics/introductory_questions/whileloop.py
@@ -0,0 +1,14 @@
+"""
+While loop- printTable (GFG):
+https://practice.geeksforgeeks.org/problems/while-loop-printtable/1
+"""
+
+
+class Solution:
+    def printTable(self, n):
+        multiplier = 10
+        while (multiplier):
+            # Code here
+            print(n * multiplier, end=" ")
+            multiplier -= 1
+        print()
