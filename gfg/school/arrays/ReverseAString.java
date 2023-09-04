fa61d16c46ea70e6f535b447be78168b218caf25
Arshad Ahmad <arshad125@proton.me>
2023-09-04 20:43:53 +0530

    new solutions added and directory changed

---
+++ gfg/school/arrays/ReverseAString.java
@@ -0,0 +1,26 @@
+package gfg.school.arrays;
+
+/*
+
+Reverse a String:
+https://practice.geeksforgeeks.org/problems/java-reverse-a-string0416/1
+
+*/
+
+public class ReverseAString {
+        public static void main(String[] args) {
+            String S = "abcdef";
+            System.out.println(reverse(S));
+        }
+
+        static String reverse(String S){
+            StringBuilder sb = new StringBuilder();
+
+            for (int i=S.length()-1; i>=0; i--){
+                char ch = S.charAt(i);
+                sb.append(ch);
+            }
+
+            return String.valueOf(sb);
+        }
+}
---
+++ gfg/school/arrays/SumOfSeries.java
@@ -0,0 +1,27 @@
+package gfg.school.arrays;
+
+/*
+
+Q: Sum Of Series
+https://practice.geeksforgeeks.org/problems/sum-of-series2811/1
+
+Input:
+n = 5
+output: 15
+Explanation: For n = 5, sum will be 15.
+1 + 2 + 3 + 4 + 5 = 15.
+
+*/
+
+public class SumOfSeries {
+    long seriesSum(int n){
+        long result = (long) n * (n+1) / 2;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SumOfSeries series = new SumOfSeries();
+        long result = series.seriesSum(15);
+        System.out.println(result);
+    }
+}
